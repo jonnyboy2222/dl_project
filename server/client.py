a13510f53612500f67488c0e1780767e5728b08a
이동훈 <98556022+donghunl33@users.noreply.github.com>
2025-06-19 16:44:35 +0900

    client updated
    client obj_result_process updated

--- server/client.py
+++ server/client.py
@@ -244,41 +244,35 @@ class ObjectResultProcessor():
         try:
             # 큐에서 최신 결과 추출
             while not obj_tcp_queue.empty():
-                uuid, json = obj_tcp_queue.get_nowait()
-                # if obj_data is not None:
-                #     self.current_detections = obj_data
+                uuid, obj_data = obj_tcp_queue.get()
 
-            # 프레임과 동일한 크기의 빈 overlay 생성
-            mask = np.zeros_like(frame, dtype=np.uint8)
-
-            # detection 결과를 mask에 그림
-            for det in self.current_detections:
-                if 'bbox' not in det:
+                if obj_data is None:
                     continue
-                x1, y1, x2, y2 = det['bbox']
-                class_name = det.get('class_name', 'object')
-                conf = det.get('confidence', 0.0)
 
-                label = f"{class_name} {conf:.2f}"
+                # 프레임과 동일한 크기의 빈 overlay 생성
+                mask = np.zeros_like(frame, dtype=np.uint8)
+
+                # detection 결과를 mask에 그림
+                if 'bbox' not in obj_data:
+                    continue
+                
+                x1, y1, x2, y2 = obj_data['bbox']
+                class_name = obj_data.get('class_name', 'object')
+
+                label = f"{class_name}"
                 color = (0, 255, 0)
 
                 cv2.rectangle(mask, (x1, y1), (x2, y2), color, 2)
                 cv2.putText(mask, label, (x1, y1 - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-            # return mask 
+                obj_result_queue.put((uuid, mask, class_name))
+
+                # return mask 
 
         except Exception as e:
             print(f"[OBJ RESULT PROCESS ERROR] {e}")
             return None
-
-        # "class_id": cls_id,
-        # "class_name": class_names[cls_id],
-        # "confidence": round(conf, 3),
-        # "bbox": [x1, y1, x2, y2]
-
-        # ['car', 'child_protection', 'construction', 'person', 'speed_limit_30', 
-        # 'speed_limit_50', 'stop_sign', 'veh_go', 'veh_goLeft', 'veh_stop', 'veh_warning']
 
 class WindowClass(QMainWindow, from_class):
     def __init__(self):
